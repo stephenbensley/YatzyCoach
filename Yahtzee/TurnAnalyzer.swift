8abda4f017c850949e5346d509cc1b32d5d1a158
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-07-14 14:08:29 -0700

    Add TurnAnalyzer.analyze

--- Yahtzee/TurnAnalyzer.swift
+++ Yahtzee/TurnAnalyzer.swift
@@ -7,12 +7,50 @@
 
 import Foundation
 
+// Represents an action that a player can take in the game of Yahtzee
+enum Action {
+    case scoreDice(ScoringOption)
+    case rollDice(DiceSelection)
+}
+
+// Combines an Action with its expected value.
+struct ActionValue {
+    var action: Action
+    var value: Double
+}
+
 // Analyzes the various roll states that occur during a turn.
 class TurnAnalyzer {
     private let diceStore: DiceStore
     private let turnValues: TurnValues
     private let state: TurnState
     private var rollValues = [[Double]]()
+    
+    // Returns all possible action values for the given state
+    func analyze(dice: Dice, rollsLeft: Int) -> [ActionValue] {
+        var analysis = [ActionValue]()
+        
+        for option in ScoringOption.allCases {
+            guard !state.used.isSet(option) else {
+                continue
+            }
+            let value = evaluateScoreAction(dice: dice, option: option)
+            analysis.append(ActionValue(action: .scoreDice(option), value: value))
+        }
+        
+        guard rollsLeft > 0 else {
+            return analysis
+        }
+        
+        for i in 0..<dice.keepOptions.count {
+            let selection = dice.keepOptions[i]
+            let kept = dice.keepResults[i]
+            let value = evaluateRollAction(dice: dice, rollsLeft: rollsLeft, kept: kept)
+            analysis.append(ActionValue(action: .rollDice(selection), value: value))
+        }
+        
+        return analysis
+    }
     
     // Returns the expected number of points scored during the remainder of the game from the
     // current state assuming optimal play.
