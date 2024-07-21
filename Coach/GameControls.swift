913b4422e3fac13787127053f39df17f47d56a7b
Stephen Bensley <76857027+stephenbensley@users.noreply.github.com>
2024-07-21 13:21:13 -0700

    Fix copyright message.

--- Coach/GameControls.swift
+++ Coach/GameControls.swift
@@ -1,8 +1,8 @@
 //
-//  ButtonsView.swift
-//  Coach
+// Copyright 2024 Stephen E. Bensley
 //
-//  Created by Stephen Bensley on 7/20/24.
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/YahtzeeCoach/blob/main/LICENSE.
 //
 
 import SwiftUI
--- Coach/GameView.swift
+++ Coach/GameView.swift
@@ -1,8 +1,8 @@
 //
-//  GameView.swift
-//  Coach
+// Copyright 2024 Stephen E. Bensley
 //
-//  Created by Stephen Bensley on 7/19/24.
+// This file is licensed under the MIT License. You may obtain a copy of the
+// license at https://github.com/stephenbensley/YahtzeeCoach/blob/main/LICENSE.
 //
 
 import SwiftUI
